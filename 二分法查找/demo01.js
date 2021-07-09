26f8023ee869ed970930131e3565a4652d85edbd
adekang <201497709@qq.com>
2021-07-09 21:36:08 +0800

    git

---
+++ 二分法查找/demo01.js
@@ -0,0 +1,34 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var search = function (nums, target) {
+  let left = 0;
+  let right = nums.length - 1;
+  while (left <= right) {
+    const mid = Math.floor(left + (right - left) / 2);
+    if (nums[mid] === target) {
+      return mid;
+    }
+    if (nums[mid] < target) {
+      left = mid + 1;
+    } else {
+      right = mid - 1;
+    }
+  }
+  return -1;
+};
+
+let nums = [-1, 0, 3, 5, 9, 12];
+let target = 9;
+
+console.log(search(nums, target));
+
+
+const findIndex = (nums, target) => {
+  return nums.findIndex(item => {
+    return item === target;
+  });
+};
+console.log(findIndex(nums, target));
