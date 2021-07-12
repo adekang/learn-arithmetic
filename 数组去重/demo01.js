d1057b3ac8b7dba0fc184e1a4508ed8739245ba0
adekang <201497709@qq.com>
2021-07-12 20:56:28 +0800

    update

--- 数组去重/demo01.js
+++
@@ -1,9 +0,0 @@
-const arr = [3, 1, 1, 2, 5, 9, 3, 0];
-
-const deduplication = arr => {
-  let map = new Map();
-  arr.forEach(x => map.set(x, 1));
-  return [...map.keys()];
-};
-
-console.log(deduplication(arr));
--- 数组去重/demo02.js
+++
@@ -1,5 +0,0 @@
-const arr = [3, 1, 1, 2, 5, 9, 3, 0];
-
-const deduplication = arr => [...new Set(arr)];
-
-console.log(deduplication(arr));
